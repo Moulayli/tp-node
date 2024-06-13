55e26ef8309cca6443d92e4f1ef377cbbf87453d
Moulaïli Saïdi <91431031+Moulayli@users.noreply.github.com>
2024-06-13 15:48:50 +0200

    Ajout : export en pdf

--- router/commandeRouter.js
+++ router/commandeRouter.js
@@ -8,5 +8,7 @@ router.get("/commande/:id_commande", controller.getById)
 router.post("/bars/:id_bar/commande", controller.addToBar)
 router.put("/commande/:id_commande", controller.update)
 router.delete("/commande/:id_commande", controller.delete)
+router.get('/commande/details/:id_commande', controller.generateCommandePDF); // Nouvelle route pour le PDF
+
 
 module.exports = router
